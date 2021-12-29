3be4092a8855af864e41319497d7fe20b826725f
MaximZoz <max591018@gmail.com>
2021-12-29 18:31:00 +0200

    работа с выпадающим списком

--- main.py
+++ main.py
@@ -3,39 +3,32 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 browser = webdriver.Chrome()
-link = "http://suninjuly.github.io/get_attribute.html"
+link = "http://suninjuly.github.io/selects1.html"
 browser.get(link)
 
 
-def calc(str_num):
-    return str(math.log(abs(12 * math.sin(int(str_num)))))
+def calc_sum(str_num1, str_num2):
+    return str((int(str_num1) + int(str_num2)))
 
 
 try:
+    # получаем числа из span
+    num1 = browser.find_element(By.ID, "num1").text
+    num2 = browser.find_element(By.ID, "num2").text
 
-    # находим число из страницы, подставляем его в формулу
-    x_element = browser.find_element(By.ID, "treasure")
-    x = x_element.get_attribute("valuex")
-    y = calc(x)
+    # складываем числа
+    res = calc_sum(num1, num2)
 
-    # вводим в input то, что получилось
-    answer = browser.find_element(By.ID, "answer")
-    answer.send_keys(y)
-
-    # отмечаем checkbox "I'm the robot"
-    robotCheckbox = browser.find_element(By.ID, "robotCheckbox")
-    robotCheckbox.click()
-
-    # выбираем radiobutton "Robots rule!"
-    robotsRule = browser.find_element(By.ID, "robotsRule")
-    robotsRule.click()
+    # выбираем мз выпадающего списка это число
+    select = Select(browser.find_element(By.ID, "dropdown"))
+    select.select_by_value(res)
 
     # кликаем на submit
     button = browser.find_element(By.TAG_NAME, "[type='submit']")
     button.click()
-
 
 finally:
     # ожидание чтобы визуально оценить результаты прохождения скрипта
