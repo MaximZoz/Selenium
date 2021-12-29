d963af6c03370b24c971a5cbc04e284b1ea58288
MaximZoz <max591018@gmail.com>
2021-12-29 23:35:07 +0200

    alert

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
-import os
+import math
 import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-link = "http://suninjuly.github.io/file_input.html"
+link = "http://suninjuly.github.io/alert_accept.html"
 browser = webdriver.Chrome()
 browser.get(link)
+
+
+def calc(str_num):
+    return str(math.log(abs(12 * math.sin(int(str_num)))))
+
 
 try:
 
     # ищем элементы в браузере
-    input1 = browser.find_element(By.XPATH, "//input[@placeholder='Enter first name']")
-    input2 = browser.find_element(By.XPATH, "//input[@placeholder='Enter last name']")
 
-    input3 = browser.find_element(By.XPATH, "//input[@placeholder='Enter email']")
+    button = browser.find_element(By.TAG_NAME, "[type='submit']")
 
-    button = browser.find_element(By.XPATH, "//button[@type='submit' and text()='Submit']")
-    file = browser.find_element(By.ID, "file")
-
-    # получаем путь к директории текущего исполняемого файла
-    current_dir = os.path.abspath(os.path.dirname(__file__))
-    file_path = os.path.join(current_dir, 'file.txt')
-
-    # заполняем обязательные поля
-    input1.send_keys("Ivan")
-    input2.send_keys("Petrov")
-    input3.send_keys("Ivan123@gmail.com")
-
-    # прикрепляем файл к форме
-    file.send_keys(file_path)
-
-    # отправляем заполненную форму с файлом
     button.click()
+
+
+    # нажимаем ок на alert
+    alert = browser.switch_to.alert
+    alert.accept()
+
+    x_element = browser.find_element(By.ID, "input_value").text
+    answer = browser.find_element(By.ID, "answer")
+    y = calc(x_element)
+    answer.send_keys(y)
+
+    button = browser.find_element(By.TAG_NAME, "[type='submit']")
+    button.click()
+
 
 finally:
     # ожидание чтобы визуально оценить результаты прохождения скрипта
